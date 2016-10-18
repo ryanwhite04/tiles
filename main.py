81984c09a30daac8187b13b9d8f071f5ce115da4
Ryan White <ryanwhite04@gmail.com>
2016-10-18 13:02:26 +0800

    Add basic boilerplate

---
+++ main.py
@@ -0,0 +1,12 @@
+from graphics import GraphWin, Circle, Point
+
+
+def main():
+    drawGrid()
+    win = GraphWin("My Circle", 100, 100)
+    c = Circle(Point(50, 50), 10)
+    c.draw(win)
+    win.getMouse()  # Pause to view result
+    win.close()  # Close window when done
+
+main()
