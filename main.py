a7595166302d9b8fbdadd7c5b9437711ec115fc4
Ryan White <ryanwhite04@gmail.com>
2016-11-04 16:52:07 +0800

    Allow player to change density and size

--- main.py
+++ main.py
@@ -2,37 +2,50 @@
 # Ryan White 20528376 and Ziggy O'Reilly
 # https://github.com/RyanWhite04/tiles
 
-from graphics import GraphWin, Point, Rectangle, Text
+from graphics import GraphWin, Point, Rectangle, Text, Entry
 from random import random
 
 
 # Density is the ratio of tiles which start with a two
 # Padding is the spacing around ui components
-def main(title='Tile Game', padding=10, density=0.5, dimensions={
+# Most is the largest size of the grid, max was taken
+
+# Rather than having the computer play, I just
+# made a function called best move with performs the computers move
+# so the user doesn't have to wait and watch, and even works mid human game
+# It's a better approach, but not in line with the instructions
+def main(title='Tiles', padding=10, density=0.5, size=5, most=10, dimensions={
     'x': 120,      # Pixels left to grid
     'y': 10,       # Pixels down to grid
     'width': 45,   # Width of tile
     'height': 45,  # Height of tile
-    'across': 5,   # Tiles across, min 3
-    'down': 5      # Tiles down, min 3
 }):
 
     # These are used to construct the board
     x = dimensions['x']
     y = dimensions['y']
-    width = padding + x + dimensions['across'] * dimensions['width']
-    height = padding + y + dimensions['down'] * dimensions['height']
 
-    # The window is made just big enough to fit all components
+    #
+    width = padding + x + most * dimensions['width']
+    height = padding + y + most * dimensions['height']
+
+    # The window is made just big enough to fit all components at max size
     win = GraphWin(title, width, height)
 
     # The squares for the game
-    grid = Grid(dimensions).fill(density)
+    grid = Grid(dimensions, size).fill(density)
 
     # Labels which show the score and state of the game
     labels = {
-        'score': Text(Point(x/2, 110), '0'),
-        'state': Text(Point(x/2, 130), 'Hey There')
+        'score': Text(Point(x/2, 140), '0'),
+        'state': Text(Point(x/2, 160), 'Hey There')
+    }
+
+    inputs = {
+        'density': Input('Density', 0.5, Point(0.7*x, 180), 4, 0, 1),
+
+        # Minimum is 3, because less and the game is stupid
+        'size': Input('Size', 5, Point(0.7*x, 200), 4, 3, most)
     }
 
     # The best, reset and exit buttons
@@ -44,7 +57,7 @@ def main(title='Tile Game', padding=10, density=0.5, dimensions={
         Button('Reset', 'green', {
             'x': [padding, x - padding],
             'y': [40, 60]
-        }, reset(win, grid, labels, density, dimensions)),
+        }, reset(win, grid, labels, inputs, dimensions)),
         Button('Exit', 'red', {
             'x': [padding, x - padding],
             'y': [70, 90]
@@ -59,24 +72,33 @@ def main(title='Tile Game', padding=10, density=0.5, dimensions={
     win.bind("<Key>", key(move(grid, labels)))
 
     # Don't quit on me!
-    draw(win, grid, labels, buttons).mainloop()
+    draw(win, grid, labels, inputs, buttons).mainloop()
 
 
 # Draw all the components
-def draw(win, grid, labels, buttons):
+def draw(win, grid, labels, inputs, buttons):
     grid.draw(win)
+    inputs['size'].draw(win)
+    inputs['density'].draw(win)
     labels['score'].draw(win)
     labels['state'].draw(win)
     [b.draw(win) for b in buttons]
+
+    if grid.over():
+        labels['state'].setText('Game Over')
     return win
 
 
 # Reset the gameboard including the grid, score, and state
-def reset(win, grid, labels, density, dimensions):
+def reset(win, grid, labels, inputs, dimensions):
     def partial():
         labels['score'].setText('0')
         labels['state'].setText('New Game')
-        grid.reset(win, density, dimensions)
+        density = inputs['density'].getValue()
+        size = inputs['size'].getValue()
+        grid.reset(win, density, size, dimensions)
+        if grid.over():
+            labels['state'].setText('Game Over')
     return partial
 
 
@@ -89,10 +111,10 @@ def click(win, grid, labels, buttons):
             for tile in grid.tiles
             if isClicked(tile.area)
         ]
-        if (clickedTiles and clickedTiles[0]):
-            move(clickedTiles[0])
-        [b.callback() for b in buttons if isClicked(b.area)]
         win.focus_set()
+        if (clickedTiles and clickedTiles[0]):
+            move(grid, labels)(clickedTiles[0])
+        return [b.callback() for b in buttons if isClicked(b.area)]
     return partial
 
 
@@ -152,31 +174,31 @@ def getClickFunction(x, y):
 
 class Grid:
 
-    def __init__(self, dimensions):
+    def __init__(self, dimensions, size):
 
-        # Alter the order of the lines so that the same move can be performed
-        # for instance, a down move is really just an up move when all the
-        # tiles are placed in reverse order in their respective lines and the
-        # line order is reversed
-        def order(direction, unordered):
-            lines = [line.tiles for line in unordered]
-            if direction == 'left' or direction == 'right':
-                # Rotate the grid clockwise 90 degrees
-                lines = [list(i) for i in zip(*lines[::-1])]
-            if direction == 'down' or direction == 'left':
-                # Reverse the grid
-                lines = [line[::-1] for line in lines[::-1]]
-
-            return [Line().update(line) for line in lines]
-
-        across = dimensions['across']
-        self.lines = [Line(dimensions, i) for i in range(across)]
+        size = int(size)
+        self.lines = [Line(dimensions, size, i) for i in range(size)]
         self.tiles = [j for i in self.lines for j in i.tiles]
         self.views = [
-            order(direction, self.lines)
+            self.order(direction)
             for direction
             in ['up', 'down', 'left', 'right']
         ]
+
+    # Alter the order of the lines so that the same move can be performed
+    # for instance, a down move is really just an up move when all the
+    # tiles are placed in reverse order in their respective lines and the
+    # line order is reversed
+    def order(self, direction):
+        lines = [line.tiles for line in self.lines]
+        if direction == 'left' or direction == 'right':
+            # Rotate the grid clockwise 90 degrees
+            lines = [list(i) for i in zip(*lines[::-1])]
+        if direction == 'down' or direction == 'left':
+            # Reverse the grid
+            lines = [line[::-1] for line in lines[::-1]]
+
+        return [Line().update(line) for line in lines]
 
     def draw(self, win):
         [line.draw(win) for line in self.lines]
@@ -193,7 +215,7 @@ class Grid:
             return positions.pop(int(len(positions) * random()))
 
         tiles = self.tiles
-
+        density = float(density)
         [tile.update(0) for tile in tiles]
         positions = list(range(len(tiles)))
         # Returns an array of the positions in tiles which should start as two
@@ -202,11 +224,20 @@ class Grid:
         [tiles[i].update(2) for i in twos]
         return self
 
-    def reset(self, win, density, dimensions):
-        across = dimensions['across']
+    # TODO Shouldn't need this, but can't figure out how to return grid from
+    # events to start using a new grid
+    def reset(self, win, density, size, dimensions):
+
+        size = int(size)
+        density = float(density)
         self.clear()
-        self.lines = [Line(dimensions, i) for i in range(across)]
+        self.lines = [Line(dimensions, size, i) for i in range(size)]
         self.tiles = [j for i in self.lines for j in i.tiles]
+        self.views = [
+            self.order(direction)
+            for direction
+            in ['up', 'down', 'left', 'right']
+        ]
         return self.fill(density).draw(win)
 
     # Shift the lines according to the rules of the gameboard
@@ -228,7 +259,9 @@ class Grid:
 
     # Return true if the game is over
     def over(self):
-        return not any([self.able(view) for view in self.views])
+        views = self.views
+        able = self.able
+        return not any([able(view) for view in views])
 
     # Check if all the lines in a view can be shifted
     def able(self, view):
@@ -238,10 +271,9 @@ class Grid:
 # A group of tiles which can be shifted and arranged according to their order
 class Line:
 
-    def __init__(self, dimensions=None, i=None):
+    def __init__(self, dimensions=None, size=None, i=None):
         if dimensions:
-            down = dimensions['down']
-            self.tiles = [Tile(dimensions, i, j) for j in range(down)]
+            self.tiles = [Tile(dimensions, size, i, j) for j in range(size)]
 
     def draw(self, win):
         [tile.draw(win) for tile in self.tiles]
@@ -309,15 +341,16 @@ class Line:
 # A square and a number with an associated value
 class Tile:
 
-    def __init__(self, dimensions, i, j):
+    # TODO make it just accept x, y coords and callback
+    def __init__(self, dimensions, size, i, j):
 
         x = dimensions['x']
         y = dimensions['y']
         width = dimensions['width']
         height = dimensions['height']
-        across = dimensions['across']
-        down = dimensions['down']
 
+        # In future might make across able to be different to down
+        across = down = size
         self.area = Rectangle(
             Point(x + width * i, y + height * j),
             Point(x + width * (i+1), y + height * (j+1)))
@@ -360,6 +393,46 @@ class Tile:
             self.direction = 'down'
         else:
             self.direction = False
+
+
+class Input:
+
+    def __init__(self, label, default, point, width, least, most):
+
+        # Because it's monospace font
+        face = 'courier'
+        self.default = default
+        self.min = least
+        self.max = most
+        self.entry = Entry(point, width)
+        self.entry.setFace(face)
+        self.entry.setText(default)
+
+        # This only works because the font face is courier (monospace)
+        # It more than double the label length, so that when it's center
+        # matches that of the input, the text aligns perfectly on the left.
+        self.label = Text(point, label + ': ' + (' ' * (6 + len(label))))
+        self.label.setFace('courier')
+
+    def draw(self, win):
+        self.entry.draw(win)
+        self.label.draw(win)
+        return self
+
+    def validate(self):
+        entry = self.entry
+        value = entry.getText()
+        try:
+            float(value)  # Check to see if it's even a number
+        except ValueError:
+            value = self.default
+            entry.setText(value)
+        if float(value) < self.min or float(value) > self.max:
+            entry.setText(self.default)
+        return self
+
+    def getValue(self):
+        return self.validate().entry.getText()
 
 
 class Button:
